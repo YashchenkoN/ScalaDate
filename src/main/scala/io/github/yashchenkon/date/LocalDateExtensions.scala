cd9702668d914730cb532481c8895c970e1ab1d1
kolyan <vkont4@gmail.com>
2017-09-15 21:57:18 +0300

    Initial commit (added LocalDate extensions)

---
+++ src/main/scala/io/github/yashchenkon/date/LocalDateExtensions.scala
@@ -0,0 +1,70 @@
+package io.github.yashchenkon.date
+
+import java.time.{LocalDate, Period}
+
+
+/**
+  * @author Mykola Yashchenko
+  */
+object LocalDateExtensions {
+
+  implicit final class LocalDateExt(ext: LocalDate) {
+    def +(duration: Period): LocalDate = {
+      ext.plus(duration)
+    }
+
+    def -(period: Period): LocalDate = {
+      ext.minus(period)
+    }
+
+    def >(localDate: LocalDate): Boolean = {
+      ext.isAfter(localDate)
+    }
+
+    def <(localDate: LocalDate): Boolean = {
+      ext.isBefore(localDate)
+    }
+
+    def ==(localDate: LocalDate): Boolean = {
+      localDate.isEqual(localDate)
+    }
+
+    def !=(localDate: LocalDate): Boolean = {
+      !(ext == localDate)
+    }
+
+    def >=(localDate: LocalDate): Boolean = {
+      ext > localDate || ext == localDate
+    }
+
+    def <=(localDate: LocalDate): Boolean = {
+      ext < localDate || ext == localDate
+    }
+  }
+
+  implicit final class PeriodExt(self: Period) {
+    def +(period: Period): Period = {
+      self.plus(period)
+    }
+
+    def -(period: Period): Period = {
+      self.minus(period)
+    }
+  }
+
+  implicit final class Day(d: Int) {
+    def day: Period = Period.ofDays(d)
+  }
+
+  implicit final class Month(m: Int) {
+    def month: Period = Period.ofMonths(m)
+  }
+
+  implicit final class Week(w: Int) {
+    def week: Period = Period.ofWeeks(w)
+  }
+
+  implicit final class Year(y: Int) {
+    def year: Period = Period.ofYears(y)
+  }
+}
---
+++ src/test/scala/io/github/yashchenkon/date/LocalDateExtensionsTest.scala
@@ -0,0 +1,79 @@
+package io.github.yashchenkon.date
+
+import java.time.LocalDate
+import LocalDateExtensions._
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * @author Mykola Yashchenko
+  */
+class LocalDateExtensionsTest extends FlatSpec with Matchers {
+
+  it should "add a day" in {
+    LocalDate.of(2017, 1, 1) + 1.day should be (LocalDate.of(2017, 1, 2))
+  }
+
+  it should "add a week" in {
+    LocalDate.of(2017, 1, 1) + 1.week should be (LocalDate.of(2017, 1, 8))
+  }
+
+  it should "add a month" in {
+    LocalDate.of(2017, 1, 1) + 1.month should be (LocalDate.of(2017, 2, 1))
+  }
+
+  it should "add a year" in {
+    LocalDate.of(2017, 1, 1) + 1.year should be (LocalDate.of(2018, 1, 1))
+  }
+
+  it should "add complex values" in {
+    LocalDate.of(2017, 1, 1) + 1.year + 1.day + 1.week + 1.month should be (LocalDate.of(2018, 2, 9))
+    LocalDate.of(2017, 1, 1) + (1.year + 1.day + 1.week + 1.month) should be (LocalDate.of(2018, 2, 9))
+  }
+
+  it should "remove a day" in {
+    LocalDate.of(2017, 1, 2) - 1.day should be (LocalDate.of(2017, 1, 1))
+  }
+
+  it should "remove a week" in {
+    LocalDate.of(2017, 1, 8) - 1.week should be (LocalDate.of(2017, 1, 1))
+  }
+
+  it should "remove a month" in {
+    LocalDate.of(2017, 2, 1) - 1.month should be (LocalDate.of(2017, 1, 1))
+  }
+
+  it should "remove a year" in {
+    LocalDate.of(2018, 1, 1) - 1.year should be (LocalDate.of(2017, 1, 1))
+  }
+
+  it should "remove complex values" in {
+    LocalDate.of(2018, 2, 9) - 1.year - 1.day - 1.week - 1.month should be (LocalDate.of(2017, 1, 1))
+    LocalDate.of(2018, 2, 9) - (1.year + 1.day + 1.week + 1.month) should be (LocalDate.of(2017, 1, 1))
+  }
+
+  it should "compare dates" in {
+    LocalDate.of(2017, 1, 2) > LocalDate.of(2017, 1, 1) should be (true)
+    LocalDate.of(2017, 1, 1) > LocalDate.of(2017, 1, 2) should be (false)
+    LocalDate.of(2017, 1, 1) > LocalDate.of(2017, 1, 1) should be (false)
+
+    LocalDate.of(2017, 1, 1) < LocalDate.of(2017, 1, 2) should be (true)
+    LocalDate.of(2017, 1, 2) < LocalDate.of(2017, 1, 1) should be (false)
+    LocalDate.of(2017, 1, 1) < LocalDate.of(2017, 1, 1) should be (false)
+
+    LocalDate.of(2017, 1, 1) == LocalDate.of(2017, 1, 1) should be (true)
+    LocalDate.of(2017, 1, 1) == LocalDate.of(2017, 1, 2) should be (false)
+
+    LocalDate.of(2017, 1, 1) != LocalDate.of(2017, 1, 2) should be (true)
+    LocalDate.of(2017, 1, 1) != LocalDate.of(2017, 1, 1) should be (false)
+
+    LocalDate.of(2017, 1, 2) >= LocalDate.of(2017, 1, 1) should be (true)
+    LocalDate.of(2017, 1, 1) >= LocalDate.of(2017, 1, 2) should be (false)
+    LocalDate.of(2017, 1, 1) >= LocalDate.of(2017, 1, 1) should be (true)
+
+    LocalDate.of(2017, 1, 1) <= LocalDate.of(2017, 1, 2) should be (true)
+    LocalDate.of(2017, 1, 2) <= LocalDate.of(2017, 1, 1) should be (false)
+    LocalDate.of(2017, 1, 1) <= LocalDate.of(2017, 1, 1) should be (true)
+  }
+
+}
